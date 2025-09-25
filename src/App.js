a4b57a5c11b61da49353ab0b0e6de2ef67ec105c
IshleenKour <kourish28@gmail.com>
2025-09-26 00:42:37 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import Alert from "./components/Alert";
-// import{
-//   BrowserRouter as Router,
-//   Routes,
-//   Route,
-// } from "react-router-dom";
+/*import
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";*/
+
 
 // let name = "Ishleen 28";
 function App() {
@@ -30,30 +31,30 @@ function App() {
       setMode("dark");
       document.body.style.backgroundColor = '#042743';
       showAlert("Dark mode has been enabled","success");
-     // document.title ="TextUtilities - DarkMode";
+     // document.title ="TxtUtilities - DarkMode";
     }
     else{
       setMode("light");
       document.body.style.backgroundColor = 'white';
       showAlert("Light mode has been enabled","success");
-      //document.title ="TextUtilities - LightMode";
+      //document.title ="TxtUtilities - LightMode";
     }
   }
   return (
     <>
-    <Router> 
-  <Navbar title ='TxtUtilities' mode={mode} toggleMode={toggleMode} AboutText = "About" />
+    {/* <Router>  */}
+  {<Navbar title = 'TxtUtilities' mode={mode} toggleMode={toggleMode} AboutText = "About" />}
   {/*<Navbar/>*/}
   <Alert alert ={alert}/>
   <div className="container my-3">
-    // <Routes>
-      // <Route exact path="/about" element ={<About mode={mode}/>}/>
-      //<Route exact path="/" element={<TextForm showAlert ={showAlert} heading = "Enter the text to analyse" mode={mode}/>}/>
-      <TextForm showAlert ={showAlert} heading = "Enter the text to analyse" mode={mode}/>}/>
-    // </Routes> {/* switch has been changed to Routes and is written in the above syntax */}
- {/* <About/>*/}
+    {/* <Routes>
+     <Route exact path="/about" element ={<About mode={mode}/>}/> 
+      <Route exact path="/" element={<TextForm showAlert ={showAlert} heading = "Enter the text to analyse" mode={mode}/>}/> */}
+      <TextForm showAlert ={showAlert} heading = "Enter the text to analyse" mode={mode}/>
+    {/* </Routes> */} {/*switch has been changed to Routes and is written in the above syntax */}
+ {/* <About/> */}
   </div>
-  // </Router>
+  {/*</Router>*/}
   </>
   );
 }
